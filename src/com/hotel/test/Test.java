3f8bc2a78c5ee299b890edca09d74e28970960eb
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2021-03-27 20:15:25 -0400

    set up project as a console-based app

---
+++ src/com/hotel/test/Test.java
@@ -0,0 +1,33 @@
+package com.hotel.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+class Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
