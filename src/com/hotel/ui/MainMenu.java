da6c2eeeba65ee5551945aca46fec2d099ad7d39
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2021-03-31 10:14:15 -0400

    cleaned up main menu

--- src/com/hotel/ui/MainMenu.java
+++ src/com/hotel/ui/MainMenu.java
@@ -38,215 +38,78 @@ public class MainMenu implements Menu {
 			
 			System.out.println();
 			System.out.println("What would you like to do?");
-			System.out.println("a: account info");
-			System.out.println("b: balances");
-			System.out.println("d: deposit");
+			System.out.println("a: account menu");
+			System.out.println("r: reservation menu");
 			System.out.println("h: help");
-			System.out.println("m: manage account");
-			System.out.println("t: transfer money");
 			System.out.println("q: quit");
-			System.out.println("w: withdrawal");
 			System.out.println();
 
 			System.out.print("Please type in your selection: ");
 			selection = scanner.nextLine();
 			System.out.println();
 			
-			// TODO change this to a switch statement?
-			
-			if (selection.equals("a") || selection.equals("A")) {
-				accountDao.viewAccountDetails(account);
-			}
-			else if (selection.equals("r") || selection.equals("R")) {
-				accountDao.viewReservations(account);
-			}
-			else if(selection.equals("h") || selection.equals("H")) {
-				help();
-			}
-			else if (selection.equals("m") || selection.equals("M")) {
-				System.out.println("You would like to manage your account.");
-				System.out.println();
-				System.out.println("Would you like to update your account information or delete your account?");
-				System.out.println("'u' for update / 'd' for delete");
-				System.out.print("Your selection: ");
-				selection = scanner.nextLine();
-				System.out.println();
-				
-				if (selection.equals("u") || selection.equals("U")) {
-					System.out.println("You would like to update your account information.");
+			switch(selection) {
+				case "a": // account menu
+					System.out.println("Account Menu");
+					System.out.println();
+					System.out.println("What would you like to do?");
+					System.out.println("v: view account details");
+					System.out.println("u: update account information");
+					System.out.println("d: delete account");
+					
+					System.out.print("Please type in your selection: ");
+					selection = scanner.nextLine();
 					System.out.println();
 					
-					System.out.println("What would you like to update?");
-					System.out.println("a: address");
-					System.out.println("c: contact information (email or phone number)");
-					System.out.println("n: name");
-					System.out.println("p: password");
-					System.out.println("u: username");
-					System.out.println();
+					switch(selection) {
+						case "v": // view account info
+							accountDao.viewAccountDetails(account);
+							break;
+						case "u": // update account
+							updateAccount(selection);
+							break;
+						case "d": // delete account
+							quit = deleteAccount(selection);
+							break;
+						default:
+							System.out.println("Invalid input; please try again.");
+							System.out.println();
+					}
+					break;
+				case "r": // reservation menu
+					// TODO reservation logic
+					System.out.println("not yet implemented");
 					
 					System.out.print("Your selection: ");
 					selection = scanner.nextLine();
-					
-					if (selection.equals("a") || selection.equals("A")) {
-						System.out.println("You have selected address");
-						System.out.println();
-						
-						String street = "";
-						while (street.equals("")) {
-							System.out.print("Street: ");
-							street = scanner.nextLine();
-							System.out.println();
-						}
-						
-						String city = "";
-						while (city.equals("")) {
-							System.out.print("City: ");
-							city = scanner.nextLine();
-							System.out.println();
-						}
-						
-						String state = "";
-						while (state.equals("")) {
-							System.out.print("State: ");
-							state = scanner.nextLine();
-							System.out.println();
-						}
-						
-						String zipcode = "";
-						while (zipcode.equals("")) {
-							System.out.print("Zip code: ");
-							zipcode = scanner.nextLine();
-							System.out.println();
-						}
-						
-						account.setFullAddress(street, city, state, zipcode);
-						
-					}
-					else if (selection.equals("c") || selection.equals("C")) {
-						System.out.println("You have selected contact information");
-						System.out.println();
-						
-						System.out.println("would you like to update your email or phone number? ('e' / 'p')");
-						System.out.print("Selection: ");
-						selection = scanner.nextLine();
-						
-						if (selection.equals("e") || selection.equals("E")) {
-							String email = "";
-							while (email.equals("")) {
-								System.out.print("Email: ");
-								email = scanner.nextLine();
-								System.out.println();
-							}
-							
-							account.setEmail(email);
-							
-						}
-						else if (selection.equals("p") || selection.equals("P")) {
-							String phoneNumber = "";
-							while (phoneNumber.equals("")) {
-								System.out.print("Phone number: ");
-								phoneNumber = scanner.nextLine();
-								System.out.println();
-							}
-							account.setPhoneNumber(phoneNumber);
-							
-						}
-						else {
-							System.out.println("Invalid selection; please try again");
-							System.out.println();
-						}
-						
-					}
-					else if (selection.equals("n") || selection.equals("N")) {
-						System.out.println("You have selected name");
-						System.out.println();
-						
-						String firstname = "";
-						while (firstname.equals("")) {
-							System.out.print("First name: ");
-							firstname = scanner.nextLine();
-							System.out.println();
-						}
-						
-						// middle name can be an empty string because not everyone has a middle name
-						String middlename = "";
-						System.out.print("Middle name (hit enter key if no middle name): ");
-						middlename = scanner.nextLine();
-						System.out.println();
-						
-						String lastname = "";
-						while (lastname.equals("")) {
-							System.out.print("Last name: ");
-							lastname = scanner.nextLine();
-							System.out.println();
-						}
-						
-						account.setFullName(firstname, middlename, lastname);
-					}
-					else if (selection.equals("p") || selection.equals("P")) {
-						System.out.println("You have selected password");
-						System.out.println();
-						
-						String password = "";
-						while (password.equals("")) {
-							System.out.print("New password: ");
-							password = scanner.nextLine();
-							System.out.println();
-						}
-						
-						account.setPassword(password);
-					}
-					else if (selection.equals("u") || selection.equals("U")) {
-						System.out.println("You have selected username");
-						System.out.println();
-						
-						String newUsername = "";
-						
-						boolean usernameTaken = true;
-						
-						while (usernameTaken == true) {
-
-							while (newUsername.equals("")) {
-								System.out.print("New username: ");
-								newUsername = scanner.nextLine();
-								System.out.println();
-								
-							}
-							
-							usernameTaken = accountDao.getAccountByUsername(newUsername);
-						}
-						
-						accountDao.updateAccountUsername(account, account.getUsername(), newUsername);
-						
-						account.setUsername(newUsername);
-						
-					}
-					else {
-						System.out.println("Invalid selection; please try again");
-						System.out.println();
-					}
-					
-					accountDao.updateAccount(account);
-					
-					System.out.println("Account information has been successfully updated.");
 					System.out.println();
-				}
-				else if (selection.equals("d") || selection.equals("D")) {
 					
-					quit = deleteAccount(selection);
-					
-				}
-				else {
+					switch(selection) {
+						case "v": // view reservation information
+							accountDao.viewReservations(account);
+							break;
+						case "u": // update existing reservation
+							// TODO implement
+							System.out.println("Not yet implemented");
+							break;
+						case "d": // delete reservation
+							// TODO implement
+							System.out.println("Not yet implemented");
+							break;
+						default:
+							System.out.println("Invalid input; please try again.");
+							System.out.println();
+					}
+					break;
+				case "h": // help menu
+					help();
+					break;
+				case "q": // quit menu
+					quit = quit(selection);
+					break;
+				default: // invalid selection
 					System.out.println("Invalid input; please try again.");
 					System.out.println();
-				}
-			}
-			else if(selection.equals("q") || selection.equals("Q")) {
-				quit = quit(selection);
-			}
-			else {
-				System.out.println("Invalid input; please try again.");
-				System.out.println();
 			}
 			
 		}
@@ -259,29 +122,216 @@ public class MainMenu implements Menu {
 		// TODO add help menu info once menu options are finalized
 		System.out.println("Help Menu");
 		System.out.println();
+		System.out.println("menu options not yet implemented");
+		System.out.println();
 	}
 	
-	public boolean deleteAccount(String selection) {
-		// TODO don't let a user delete their account if they still have hotel reservations set
+	public void updateAccount(String selection) {
+		// TODO implement and fix
 		
-		System.out.println("Are you sure you want to delete your account? Type 'DELETE ACCOUNT' to confirm.");
-		System.out.println("Hit the enter key to cancel account deletion.");
+		System.out.println("You would like to update your account information.");
+		System.out.println();
+		
+		System.out.println("What would you like to update?");
+		System.out.println("a: address");
+		System.out.println("c: contact information (email or phone number)");
+		System.out.println("n: name");
+		System.out.println("p: password");
+		System.out.println("u: username");
+		System.out.println();
+		
 		System.out.print("Your selection: ");
 		selection = scanner.nextLine();
 		System.out.println();
 		
-		if (selection.equals("DELETE ACCOUNT")) {
-			accountDao.deleteAccount(account);
-			
-			System.out.println("Account successfully deleted. Have a nice day!");
+		if (selection.equals("a")) {
+			System.out.println("You have selected address");
 			System.out.println();
 			
-			return true;
+			String street = "";
+			while (street.equals("")) {
+				System.out.print("Street: ");
+				street = scanner.nextLine();
+				System.out.println();
+			}
+			
+			String city = "";
+			while (city.equals("")) {
+				System.out.print("City: ");
+				city = scanner.nextLine();
+				System.out.println();
+			}
+			
+			String state = "";
+			while (state.equals("")) {
+				System.out.print("State: ");
+				state = scanner.nextLine();
+				System.out.println();
+			}
+			
+			String zipcode = "";
+			while (zipcode.equals("")) {
+				System.out.print("Zip code: ");
+				zipcode = scanner.nextLine();
+				System.out.println();
+			}
+			
+			account.setFullAddress(street, city, state, zipcode);
+			
+		}
+		else if (selection.equals("c")) {
+			System.out.println("You have selected contact information");
+			System.out.println();
+			
+			System.out.println("would you like to update your email or phone number? ('e' / 'p')");
+			System.out.print("Selection: ");
+			selection = scanner.nextLine();
+			
+			if (selection.equals("e") || selection.equals("E")) {
+				String email = "";
+				while (email.equals("")) {
+					System.out.print("Email: ");
+					email = scanner.nextLine();
+					System.out.println();
+				}
+				
+				account.setEmail(email);
+				
+			}
+			else if (selection.equals("p")) {
+				String phoneNumber = "";
+				while (phoneNumber.equals("")) {
+					System.out.print("Phone number: ");
+					phoneNumber = scanner.nextLine();
+					System.out.println();
+				}
+				account.setPhoneNumber(phoneNumber);
+				
+			}
+			else {
+				System.out.println("Invalid selection; please try again");
+				System.out.println();
+			}
+			
+		}
+		else if (selection.equals("n")) {
+			System.out.println("You have selected name");
+			System.out.println();
+			
+			String firstname = "";
+			while (firstname.equals("")) {
+				System.out.print("First name: ");
+				firstname = scanner.nextLine();
+				System.out.println();
+			}
+			
+			// middle name can be an empty string because not everyone has a middle name
+			String middlename = "";
+			System.out.print("Middle name (hit enter key if no middle name): ");
+			middlename = scanner.nextLine();
+			System.out.println();
+			
+			String lastname = "";
+			while (lastname.equals("")) {
+				System.out.print("Last name: ");
+				lastname = scanner.nextLine();
+				System.out.println();
+			}
+			
+			account.setFullName(firstname, middlename, lastname);
+		}
+		else if (selection.equals("p")) {
+			System.out.println("You have selected password");
+			System.out.println();
+			
+			String password = "";
+			while (password.equals("")) {
+				System.out.print("New password: ");
+				password = scanner.nextLine();
+				System.out.println();
+			}
+			
+			account.setPassword(password);
+		}
+		else if (selection.equals("u")) {
+			System.out.println("You have selected username");
+			System.out.println();
+			
+			String newUsername = "";
+			
+			boolean usernameTaken = true;
+			
+			while (usernameTaken == true) {
+
+				while (newUsername.equals("")) {
+					System.out.print("New username: ");
+					newUsername = scanner.nextLine();
+					System.out.println();
+					
+				}
+				
+				usernameTaken = accountDao.getAccountByUsername(newUsername);
+			}
+			
+			account.setUsername(newUsername);
+			
 		}
 		else {
-			System.out.println("Account deletion cancelled");
+			System.out.println("Invalid selection; please try again");
 			System.out.println();
-			return false;
+		}
+		
+		accountDao.updateAccount(account);
+		
+		System.out.println("Account information has been successfully updated.");
+		System.out.println();
+	}
+	
+	public void updateUsername(String selection) {
+		
+	}
+	
+	public void updatePassword(String selection) {
+		
+	}
+	
+	public void updateName(String selection) {
+		
+	}
+	
+	public void updateAddress(String selection) {
+		
+	}
+	
+	public void updateEmail(String selection) {
+		
+	}
+	
+	public void updatePhone(String selection) {
+		
+	}
+	
+	public boolean deleteAccount(String selection) {
+		// TODO don't let a user delete their account if they still have hotel reservations set
+		// TODO OR make sure they know deleting account will cancel all existing reservations
+		
+		System.out.println("Are you sure you want to delete your account? Type 'DELETE ACCOUNT' to confirm.");
+		System.out.println("Hit any key to cancel account deletion.");
+		System.out.print("Your selection: ");
+		selection = scanner.nextLine();
+		System.out.println();
+		
+		switch(selection) {
+			case "DELETE ACCOUNT":
+				accountDao.deleteAccount(account);
+				
+				System.out.println("Account successfully deleted. Have a nice day!");
+				System.out.println();
+				
+			default:
+				System.out.println("Account deletion cancelled");
+				System.out.println();
+				return false;
 		}
 		
 	}
@@ -289,21 +339,21 @@ public class MainMenu implements Menu {
 	public boolean quit(String selection) {
 		System.out.println("Are you sure you want to quit? (y / n)");
 		System.out.print("Your selection: ");
-		String confirmationSelection = scanner.nextLine();
+		selection = scanner.nextLine();
 		System.out.println();
-
-		if (confirmationSelection.equals("y") || confirmationSelection.equals("Y")) {
-			System.out.println("Thank you and have a great day!");
-			return true;
+		
+		switch(selection) {
+			case "y":
+				System.out.println("Thank you and have a great day!");
+				return true;
+			case "n":
+				return false;
+			default:
+				System.out.println("Invalid selection; please try again.");
+				System.out.println();
+				return false;
 		}
-		else if (confirmationSelection.equals("n") || confirmationSelection.equals("N")) {
-			return false;
-		}
-		else {
-			System.out.println("Invalid selection; please try again.");
-			System.out.println();
-			return false;
-		}
+		
 	}
 
 	@Override
