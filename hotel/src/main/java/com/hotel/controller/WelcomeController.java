ab33bd5a3d54c045cb07013accf7bf45d9380bb8
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2021-04-02 11:31:09 -0400

    added several files to the console-based app to further encapsulate functionality; created a separate app to be the REST API

---
+++ hotel/src/main/java/com/hotel/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String world() {
+		return "Welcome to the Hotel API!";
+	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "hello world!";
+	}
+	
+}
