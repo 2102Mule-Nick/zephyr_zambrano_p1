0ee668890420fa51c4ebe181b925a7c28aa3baae
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2021-04-06 10:54:00 -0400

    added new exception

---
+++ hotel/src/main/java/com/hotel/exception/ReservationNotFound.java
@@ -0,0 +1,30 @@
+package com.hotel.exception;
+
+public class ReservationNotFound extends Exception {
+	
+	public ReservationNotFound() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReservationNotFound(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReservationNotFound(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReservationNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReservationNotFound(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
